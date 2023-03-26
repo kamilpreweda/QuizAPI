33263ffbe2435a0305bf6c176c789bb61d62d867
Kamil Preweda <preweda.kamil@o2.pl>
2023-03-26 14:55:01 +0200

    Dodaj pliki projektów.

---
+++ QuizAPI/Controllers/Models/Participant.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizAPI.Controllers.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int ParticipantId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+    }
+}
